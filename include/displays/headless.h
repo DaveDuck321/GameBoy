b9e7f8b03c1a108676eaa69fef0a1aff43cbaabd
Thomas Grant <tg443@cam.ac.uk>
2020-04-08 18:28:03 -0500

    Headless display for debugging

---
+++ include/displays/headless.h
@@ -0,0 +1,20 @@
+#ifndef headless_h
+#define headless_h
+
+#include <memory>
+
+#include "display.hpp"
+
+class Headless: public Display
+{
+    private:
+    public:
+    Headless();
+    ~Headless();
+
+    void clearScreen() const override {};
+
+    void drawTile(Tile tile, int screenX, int screenY) const {};
+};
+
+#endif
