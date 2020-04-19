72226af4870dac95baf67306c8383090c52ab655
Thomas Grant <tg443@cam.ac.uk>
2020-04-18 23:11:37 -0500

    Organized io sound registers

--- src/io_manager.cpp
+++ src/io_manager.cpp
@@ -68,9 +68,9 @@ void IO_Manager::ioWrite(uint16_t addr, uint8_t value)
 {
     switch (addr)
     {
-    case 0xFF04:
-        //DIV -- Divider Register (cannot write data)
-        throw std::runtime_error("DIV write unsupported");
+    case 0xFF00:
+        //P1 Joypad
+        memory[addr-IO_OFFSET] = 0xC0|(value&0x30);
         break;
     case 0xFF02:
         //SC -- SIO control (r/w)
@@ -78,6 +78,18 @@ void IO_Manager::ioWrite(uint16_t addr, uint8_t value)
         std::cout<<memory[SERIAL_DATA];
         memory[addr-IO_OFFSET] = value;
         break;
+    case 0xFF04:
+        //DIV -- Divider Register (cannot write data)
+        throw std::runtime_error("DIV write unsupported");
+        break;
+    case 0xFF05:
+        // TIMA -- Timer counter (R/W)
+        // Should only update timers between instructions when accessed
+        updateTimers();
+        memory[addr-IO_OFFSET] = value;
+        break;
+    
+    // Special LCD registers
     case 0xFF41:
         // LCD Status Register
         memory[LCD_STAT] = 0x80|(memory[LCD_STAT]&0x07)|(value&0x78);
@@ -85,30 +97,20 @@ void IO_Manager::ioWrite(uint16_t addr, uint8_t value)
     case 0xFF44:
         //LY -- Scroll Y (r)
         throw std::runtime_error("Cannot write to LY @ 0xFF44");
-    case 0xFF4B:
-        //std::cout << "Write to window x: " << (int)value << std::endl;
-        memory[addr-IO_OFFSET] = value;
-        break;
+
+    // Sound registers
     case 0xFF26:
         //NR52 - Sound on/off
         // All bits are read only except 7 (Sound on/off)
         if((value&0x80))    powerUpAPU();
         else                powerDownAPU();
         break;
-    case 0xFF00:
-        //P1 Joypad
-        memory[addr-IO_OFFSET] = 0xC0|(value&0x30);
-        break;
-    case 0xFF05:
-        // Should only update timers between instructions when accessed
-        updateTimers();
-        memory[addr-IO_OFFSET] = value;
-        break;
     case 0xFF10 ... 0xFF25:
         // All Sound Control registers
         // Writing to registers should only be possible when APU is powered
         if(!(memory[NR52_REG]&0x80)) break;
-    //Intentional fall through
+        //Intentional fall through
+
     default:
         // Most IO actions don't require immediate action, deal with it later
         memory[addr-IO_OFFSET] = value;
@@ -121,7 +123,7 @@ uint8_t IO_Manager::ioRead(uint16_t addr)
     switch(addr)
     {
     case 0xFF00: {
-        // Joypad TODO: keypresses
+        // P1/JOYP -- Joypad read
         uint8_t value = 0xFF;
         if(!(memory[addr-IO_OFFSET]&0x10))
         {
@@ -136,34 +138,48 @@ uint8_t IO_Manager::ioRead(uint16_t addr)
         return value;
     }
     case 0xFF46:
+        // DMA - DMA Transfer and Start Address (W)
         // DMA reads are never allowed
+        // There's probably an error somewhere else so throw
         throw std::runtime_error("DMA read not permitted!");
 
     //Sound
     case 0xFF10:
+        // NR10 - Channel 1 Sweep register (R/W)
         return 0x80|memory[addr-IO_OFFSET];
     case 0xFF11: case 0xFF16:
+        // NR11 - Channel 1 Sound length
         return 0x3F|memory[addr-IO_OFFSET];
-    case 0xFF13: case 0xFF15: case 0xFF18: case 0xFF1B: case 0xFF1D: case 0xFF1F: case 0xFF20:
+    case 0xFF13: case 0xFF18: case 0xFF1B: case 0xFF1D: case 0xFF20:
+        // NR13, NR23, NR31, NR33, NR41 (W)
+        // All write only so ignore read request
         return 0xFF;
     case 0xFF14: case 0xFF19: case 0xFF23: case 0xFF1E:
+        // NR14, NR24, NR44, NR34 (R/W)
+        // All registers control channel frequency
+        // Bit 0-2 and 7 are write only, bits 3-5 are invalid
         return 0xBF|memory[addr-IO_OFFSET];
     case 0xFF1A:
+        // NR30 - Channel 3 Sound on/off (R/W)
         return 0x7F|memory[addr-IO_OFFSET];
     case 0xFF1C:
+        // NR32 - Channel 3 Select output level (R/W)
         return 0x9F|memory[addr-IO_OFFSET];
     case 0xFF26:
+        // NR52 - Sound on/off
         //TODO: real implementation
         return 0x70|memory[addr-IO_OFFSET];
-    case 0xFF27 ... 0xFF2F:
-        // Invalid memory, should always give 0xFF
+    case 0xFF27 ... 0xFF2F: case 0xFF15: case 0xFF1F:
+        // Invalid memory, should always return 0xFF
         return 0xFF;
 
-    //Video
+
     case 0xFF04: case 0xFF05:
-        // Timers lazy update
+        // Timer registers: DIV, TIMA
         // Should only update timers between instructions when accessed
-        updateTimers();
+        updateTimers(); // Timers lazy update
+        //Intentional fall through
+
     default:
         return memory[addr-IO_OFFSET];
     }
