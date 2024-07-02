454d0525dc8cba67daf11c0cf7af3d87cc9a3067
Tom Grant <thomas.grant.duck321@gmail.com>
2024-07-02 01:42:44 +0100

    Make the GDB helper script path agnostic

--- gb_with_gdb.sh
+++ gb_with_gdb.sh
@@ -1,7 +1,9 @@
 port=$((1024 + ($$ % 1000)))
 
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Launch the emulator
-(trap '' INT; exec ./gb.out --listen $port)&
+(trap '' INT; exec $script_dir/gb.out --listen $port)&
 gb_pid=$!
 echo "emulator pid $gb_pid"
 trap "kill $gb_pid" EXIT
